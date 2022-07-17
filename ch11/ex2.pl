bf190d08b6be6f72db58da72cbda0a9765ea8f6e
Ashlen <eurydice@riseup.net>
2022-07-17 15:42:17 -0600

    clean up and give usage subroutine

--- ch11/ex2.pl
+++ ch11/ex2.pl
@@ -13,15 +13,30 @@ use v5.14; # character set modifiers
 # Would rather not use an external module, but the book said to.
 use DateTime;
 
+use File::Basename;
 
-my $given_year = $ARGV[0];
+my $program_name = fileparse $0;
+
+sub usage {
+	die <<EOF;
+A script to calculate the time between two dates in years, months, and
+days.
+
+$program_name [yyyy] [mm] [dd]
+EOF
+}
+
+
+@ARGV or usage;
+
+my $given_year = shift;
 $given_year =~ /\A\d{4}\z/a or die "The first argument must be a year with four digits.\n";
 
-my $given_month = $ARGV[1];
-$given_month =~ /\A\d+\z/a or die "The second argument must be a month as an integer.\n";
+my $given_month = shift;
+$given_month =~ /\A\d{1,2}\z/a or die "The second argument must be a month as an integer.\n";
 
-my $given_day = $ARGV[2];
-$given_day =~ /\A\d+\z/a or die "The third argument must be a day as an integer.\n";
+my $given_day = shift;
+$given_day =~ /\A\d{1,2}\z/a or die "The third argument must be a day as an integer.\n";
 
 
 my $datetime_now = DateTime->today();
